44181b32a65c4b0375c552242097d0a4f0a565e7
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-02-26 22:06:59 -0300

    Smaller fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true, // causes double render on component mount (dev)
 }
 
 export default nextConfig
