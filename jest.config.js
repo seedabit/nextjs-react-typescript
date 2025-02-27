195440ca5ac762c638e62c08b3348748667eea96
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-02-26 22:06:39 -0300

    Jest fix

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'babel-jest',
-    },
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
