435f6e79f0892c607b884cdf2879de06878970e4
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-02 18:37:30 -0300

    Image changed on not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function NotFound() {
     return (
-        <div>
+        <div className='w-full h-screen flex items-center justify-center space-x-10'>
+            <Image src='/images/seed-a-mascot.svg' width={200} height={350} alt='Seed a Bit - mascote' />
+            div
             <h2>Not Found</h2>
             <p>Could not find requested resource</p>
             <Link href="/">Return Home</Link>
